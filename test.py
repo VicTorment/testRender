be388c8f03f002716115275ac4a4a9359c27a378
Sebastian <sebastian@schroeder.wtf>
2023-11-16 09:19:54 +0100

    edit path

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Whishlist", page_icon=":gift:", layout="wide")
 
 
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-css_file =  current_dir /"Styles" / "main.css"
+css_file =  current_dir /"styles" / "main.css"
 
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
@@ -18,8 +18,8 @@ data = toml.load(current_dir / ".streamlit" / "config.toml")
 st.header('Basses ønskeliste')
 
 whishes = {('En fin paraply', 'https://www.paraplybutik.dk/butik/klassisk-paraply/groen-paraply-traehaandtag/'),
-           ('noget andet', 'www.somewhere.com')
-           }
+            ('noget andet', 'www.somewhere.com')
+            }
 
 for whish in whishes:
     st.write(f'{whish[0]} se more at {whish[1]}')
